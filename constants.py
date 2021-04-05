ecc0e5b83b6da7e350c35c8c24fdcfc347d8d2bd
Gavination <gavinbauman1322@gmail.com>
2021-04-05 18:20:41 -0400

    update constants

--- constants.py
+++ constants.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python
 
-PROGRAMS_LIST = ["Overwatch.exe", "r5apex.exe", "GameOverlayUI.exe", "starwarsbattlefrontii.exe"]
+PROGRAMS_LIST = ["Hades.exe","Overwatch.exe", "r5apex.exe", "GameOverlayUI.exe", "starwarsbattlefrontii.exe"]
